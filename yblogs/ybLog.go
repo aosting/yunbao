b24b16990c8821df4a5de45c4b64212fc6843f58
张雪源 <zhangxueyuan@zhangxueyuandeMacBook-Pro.local>
2018-01-22 13:46:23 +0800

    worklog

--- yblogs/ybLog.go
+++ yblogs/ybLog.go
@@ -2,10 +2,25 @@ package yblogs
 
 import (
 	"log"
+	"time"
+	"strconv"
 )
 
 var (
 	RELEASE = true
+)
+
+const (
+	PLATFORM_SSP = "ssp-proxy"
+
+	PLATFORM_NEWLAND = "newland"
+
+	PLATFORM_ADX_GDT = "adx-gdt"
+	PLATFORM_ADX_PP  = "adx-pp"
+	PLATFORM_ADX_KS  = "adx-ks"
+	PLATFORM_ADX_yyb = "adx-yyb"
+
+	WORKLOG_STORE_NAME = "nw-sys-module-log"
 )
 
 func SetRelease(isRelease bool) {
@@ -46,4 +61,31 @@ func WORKLOG(businessName string, requestId string, ms int, responseCode string,
 		result = "WORKSUCC"
 	}
 	log.Println(businessName, requestId, ms, responseCode, result, v)
+}
+
+func WORKLOG2(platform string, requestId string, slotId string, responseCode string, ms int, Succ bool, msg string) {
+	result := "WORKFAIL"
+	if Succ {
+		result = "WORKSUCC"
+	}
+
+	tmp := make(map[string]string)
+	tmp["request_timestamp"] = time.Now().Format("20060102150405")
+	tmp["hour"] = time.Now().Format("15")
+	tmp["platform"] = platform
+	tmp["requestId"] = requestId
+	tmp["slotId"] = slotId
+	tmp["responseCode"] = responseCode
+	tmp["ms"] = strconv.Itoa(ms)
+	tmp["result"] = result
+	tmp["msg"] = msg
+
+	outLog := OutLogMap{
+		LogMap: tmp,
+	}
+	error := PushLogQueue(WORKLOG_STORE_NAME, outLog)
+	if error != nil {
+		WARN(error)
+	}
+
 }
